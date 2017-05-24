47d41b475806cbea3ec3e4375f7655079ea04ccd
procedure2012 <procedure2012@hotmai.com>
2017-05-24 15:46:52 +0800

    width=0 in background.js?

--- background.js
+++ background.js
@@ -30,14 +30,143 @@ document.querySelector('#selectphoto').onchange = function(e){
     //var myurl = window.url.createObjectURL(selected);
    //alert(selected.innerHTML);
     // ...
-	var img = new Image();
-        img.onload = function() {
-            var ctx = document.getElementById('canvas').getContext('2d');
-            ctx.canvas.width = img.width;
-            ctx.canvas.height = img.height;
-            ctx.drawImage(img, 0, 0);
 
-			alert(decode());
-        };
-        img.src = selected[0].src;
+	var img = new Image();
+	img.src = selected[0].src;
+	alert(img.width)
+	alert(decode(img));
 }
+
+// decode the image and display the contents if there is anything
+var decode = function(img) {
+    var password = null;
+    var passwordFail = 'Password is incorrect or there is nothing here.';
+
+    // decode the message with the supplied password
+	var canvas = document.createElement('canvas');
+	var ctx = canvas.getContext('2d');
+	ctx.canvas.width = img.width;
+	ctx.canvas.height = img.height;
+	ctx.drawImage(img, 0, 0);
+	alert(img.width)
+	alert(ctx.canvas.width)
+    var imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+    var message = decodeMessage(imgData.data, sjcl.hash.sha256.hash(password));
+
+    // try to parse the JSON
+    var obj = null;
+    try {
+        obj = JSON.parse(message);
+    } catch (e) {
+        // display the "choose" view
+
+        document.getElementById('choose').style.display = 'block';
+        document.getElementById('reveal').style.display = 'none';
+
+        if (password.length > 0) {
+            alert(passwordFail);
+        }
+    }
+
+    // display the "reveal" view
+    if (obj) {
+        //document.getElementById('choose').style.display = 'none';
+        //document.getElementById('reveal').style.display = 'block';
+
+        // decrypt if necessary
+        if (obj.ct) {
+            try {
+                obj.text = sjcl.decrypt(password, message);
+            } catch (e) {
+                alert(passwordFail);
+            }
+        }
+
+        // escape special characters
+        var escChars = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            '\'': '&#39;',
+            '/': '&#x2F;',
+            '\n': '<br/>'
+        };
+        var escHtml = function(string) {
+            return String(string).replace(/[&<>"'\/\n]/g, function (c) {
+                return escChars[c];
+            });
+        };
+        //document.getElementById('messageDecoded').innerHTML = escHtml(obj.text);
+		return obj.text;
+	}
+};
+
+// returns a 1 or 0 for the bit in 'location'
+var getBit = function(number, location) {
+   return ((number >> location) & 1);
+};
+
+// sets the bit in 'location' to 'bit' (either a 1 or 0)
+var setBit = function(number, location, bit) {
+   return (number & ~(1 << location)) | (bit << location);
+};
+
+// returns the next 2-byte number
+var getNumberFromBits = function(bytes, history, hash) {
+    var number = 0, pos = 0;
+    while (pos < 16) {
+        var loc = getNextLocation(history, hash, bytes.length);
+        var bit = getBit(bytes[loc], 0);
+        number = setBit(number, pos, bit);
+        pos++;
+    }
+    return number;
+};
+
+// gets the next location to store a bit
+var getNextLocation = function(history, hash, total) {
+    var pos = history.length;
+    var loc = Math.abs(hash[pos % hash.length] * (pos + 1)) % total;
+    while (true) {
+        if (loc >= total) {
+            loc = 0;
+        } else if (history.indexOf(loc) >= 0) {
+            loc++;
+        } else if ((loc + 1) % 4 === 0) {
+            loc++;
+        } else {
+            history.push(loc);
+            return loc;
+        }
+    }
+};
+
+// returns the message encoded in the CanvasPixelArray 'colors'
+var decodeMessage = function(colors, hash) {
+    // this will store the color values we've already read from
+    var history = [];
+
+    // get the message size
+    var messageSize = getNumberFromBits(colors, history, hash);
+
+    // exit early if the message is too big for the image
+    if ((messageSize + 1) * 16 > colors.length * 0.75) {
+        return '';
+    }
+
+    // exit early if the message is above an artificial limit
+    if (messageSize === 0 || messageSize > maxMessageSize) {
+        return '';
+    }
+
+    // put each character into an array
+    var message = [];
+    for (var i = 0; i < messageSize; i++) {
+        var code = getNumberFromBits(colors, history, hash);
+        message.push(String.fromCharCode(code));
+    }
+
+    // the characters should parse into valid JSON
+    return message.join('');
+};
